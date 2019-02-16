6c57793ac6ee5ad259407fcc25c80bd54654c23c
tchanu <t.chanukvadze@gmail.com>
2019-02-17 00:53:16 +0400

    create dht-sensor

---
+++ packages/dht-sensor/index.js
@@ -0,0 +1,40 @@
+const EventEmitter = require('events');
+const sensor = require('node-dht-sensor');
+
+/**
+ * @module DhtSensor
+ * @extends EventEmitter
+ * @property {number} gpio
+ * @property {boolean} debug
+ */
+class DhtSensor extends EventEmitter {
+  constructor(gpio = 4, interval = 5000) {
+    super();
+    this.GPIO = gpio;
+
+    this.read();
+    setInterval(() => this.read(), interval);
+  }
+
+  /**
+   * @description read data from sensor
+   */
+  read() {
+    sensor.read(22, this.GPIO, (err, temp, humidity) => {
+      if (err) {
+        if (this.debug) console.trace(err);
+        this.emit('error', err);
+        return;
+      }
+
+      if (this.debug) console.info(temp, humidity);
+
+      this.emit('data', {
+        temp,
+        humidity,
+      });
+    });
+  }
+}
+
+module.exports = DhtSensor;
